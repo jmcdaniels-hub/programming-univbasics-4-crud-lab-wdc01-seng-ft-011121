06e2d5deb68df6e5755d9dc0b13423c96cf58d94
Jade McDaniels <jademcdaniels81@gmail.com>
2020-12-24 05:47:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,5 +42,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+ my_array = ["array", "array", "array", "array"]
+ my_array[4] = "totally"
 end
